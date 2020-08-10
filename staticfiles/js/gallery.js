667539279913a197497e4f56771c2500cea6f9ab
baronbr <brahiam.baron.moreno@outlook.com>
2020-08-10 18:25:25 -0400

    collectstatic added to this push

--- staticfiles/js/gallery.js
+++ staticfiles/js/gallery.js
@@ -11,6 +11,9 @@ $(function() {
     });
     });
 
-    $(function () {
-        $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-        });
+
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
+        });     
